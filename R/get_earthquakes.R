d619de76e1f7cad1f5bb19a5caff5449d15c0c9d
Andy <andypicke@gmail.com>
2024-07-02 12:41:35 -0600

    start new function

---
+++ R/get_earthquakes.R
@@ -0,0 +1,11 @@
+#' Retrieve earthquake data from USGS API
+#'
+#'
+get_earthquakes <- function(format = "csv"){
+
+# https://earthquake.usgs.gov/fdsnws/event/1/
+base_url <- "https://earthquake.usgs.gov/fdsnws/event/1/"
+
+
+
+}
